bf06edcaa4b592f5903e67e2ef3f6578958fe43e
Sam Anderson <sam@pax.com>
2017-05-15 10:52:44 -0700

    Install pip

--- install_programs.sh
+++ install_programs.sh
@@ -3,3 +3,6 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo apt-get install gimp inkscape compizconfig-settings-manager python-setuptools python-dev build-essential
+
+#install pip
+sudo easy_install pip
