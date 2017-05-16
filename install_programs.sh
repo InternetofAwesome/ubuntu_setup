351fd83682f50d9a60e7fe267f329095d9223e20
Sam Anderson <sam@pax.com>
2017-05-15 17:20:29 -0700

    Add root check and make install executable

--- install_programs.sh
+++ install_programs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
-sudo apt-get install gimp inkscape compizconfig-settings-manager python-setuptools python-dev build-essential minicom git gconf-editor dconf-editor
+
+if [ "$(whoami)" != "root" ]; then
+	echo "you must run this as root"
+	exit
+fi
+
+apt-get update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get install -y gimp inkscape compizconfig-settings-manager python-setuptools python-dev build-essential minicom git gconf-editor dconf-editor
 
 #install pip
-sudo easy_install pip
+easy_install pip
 
 dconf load /org/compiz/profiles/unity/plugins/grid/ < ./grid-settings.conf
 gconftool-2 --load ./lock_screen_key.xml
