a024caaeeed3ca82131b3ede8dbe2950e409ebde
Sam Anderson <sam@pax.com>
2017-05-15 17:14:27 -0700

    Add hotkey settings for grid, some packages

---
+++ README.md
@@ -0,0 +1,12 @@
+# README  
+## Save Restore DCONF values
+
+Save  
+dconf dump <dconf path> > mysettings  
+  
+Load  
+dconf load <dconf path> < mysettings  
+
+
+## Save restore gconf values  
+https://docs.oracle.com/cd/E36784_01/html/E36853/glmrh.html  
--- install_programs.sh
+++ install_programs.sh
@@ -2,7 +2,11 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
-sudo apt-get install gimp inkscape compizconfig-settings-manager python-setuptools python-dev build-essential
+sudo apt-get install gimp inkscape compizconfig-settings-manager python-setuptools python-dev build-essential minicom git gconf-editor dconf-editor
 
 #install pip
 sudo easy_install pip
+
+dconf load /org/compiz/profiles/unity/plugins/grid/ < ./grid-settings.conf
+gconftool-2 --load ./lock_screen_key.xml
+
