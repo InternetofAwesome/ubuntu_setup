7af18a42f7c07062fcaddabe55b48ffb3b30773a
Sam Anderson <sam@pax.com>
2017-05-30 16:17:01 -0700

    add eclipse

--- install_programs.sh
+++ install_programs.sh
@@ -10,12 +10,18 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 #add sublime ppa
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
+
+#add java ppa
+sudo add-apt-repository ppa:webupd8team/java -y
+#say yes to EULAs.
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-apt-get install -y gimp inkscape compizconfig-settings-manager python-setuptools python-dev build-essential minicom git gconf-editor dconf-editor google-chrome-stable submlime-text-installer curl gparted
+apt-get install -y gimp inkscape compizconfig-settings-manager python-setuptools python-dev build-essential minicom git gconf-editor dconf-editor google-chrome-stable submlime-text-installer curl gparted oracle-java8-installer
 
 #install pip
 easy_install pip
@@ -31,3 +37,8 @@ dpkg -i ./draftsight.deb
 #Move my application shortcuts to where they should go
 cp ./org.gnome.Screenshot.desktop ~/.local/share/applications/screenshot.desktop
 
+#install eclipse
+curl -k https://raw.githubusercontent.com/budhash/install-eclipse/master/install-eclipse > install-eclipse
+chmod +x install-eclipse
+./install-eclipse /opt/eclipse
+desktop-file-install ./eclipse.desktop
