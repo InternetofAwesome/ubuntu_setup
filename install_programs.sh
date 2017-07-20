ca9d587dc3abe9a4c390bc1489bdb5a687de8e42
Sam Anderson <sam@pax.com>
2017-07-20 14:36:40 -0700

    Add airplay streaming (experimental)

--- install_programs.sh
+++ install_programs.sh
@@ -5,26 +5,53 @@ if [ "$(whoami)" != "root" ]; then
 	exit
 fi
 
+PKGS="gimp \
+	inkscape \
+	compizconfig-settings-manager \
+	python-setuptools \
+	python-dev \
+	build-essential \
+	minicom \
+	git \
+	gconf-editor \
+	dconf-editor \
+	curl \
+	gparted"
+
 #add chrome ppa
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+PKGS+=" google-chrome-stable"
 
 #add sublime ppa
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
+PKGS+=" sublime-text-installer"
 
 #add java ppa
 sudo add-apt-repository ppa:webupd8team/java -y
 #say yes to EULAs.
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+PKGS+=" oracle-java8-installer"
 
 #arm compiler ppas
 sudo add-apt-repository ppa:team-gcc-arm-embedded/ppa -y
+PKGS+=" gcc-arm-embedded"
+
+#airplay casting ability
+ver=$(curl http://jcenter.bintray.com/org/jmdns/jmdns/maven-metadata.xml | sed  -n 's/.\+latest[^0-9]\+\([0-9\.-]\+\)..latest./\1/p')
+wget http://jcenter.bintray.com/org/jmdns/jmdns/$ver/jmdns-$ver.jar -O ~/jmdns.jar
+ver=$(curl http://repo2.maven.org/maven2/org/slf4j/slf4j-nop/maven-metadata.xml | sed  -n 's/.\+latest[^0-9-]\+\([0-9a-zA-Z\.-]\+\)..latest./\1/p')
+wget http://repo2.maven.org/maven2/org/slf4j/slf4j-nop/$ver/slf4j-nop-$ver.jar -O ~/slf4j-nop.jar
+wget http://repo2.maven.org/maven2/org/slf4j/slf4j-api/1.8.0-alpha2/slf4j-api-1.8.0-alpha2.jar -O ~/slf4j-api.jar
+wget https://github.com/$(wget https://github.com/jamesdlow/open-airplay/releases/latest -O - | egrep '/.*/.*/.*jar' -o)
+mv ./airplay.jar ~/airplay.jar
+desktop-file-install ./open-airplay.desktop
 
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-apt-get install -y gimp inkscape compizconfig-settings-manager python-setuptools python-dev build-essential minicom git gconf-editor dconf-editor google-chrome-stable submlime-text-installer curl gparted oracle-java8-installer gcc-arm-embedded
+apt-get install -y $PKGS
 
 #install pip
 easy_install pip
